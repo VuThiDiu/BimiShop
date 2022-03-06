00eec4dce8e003646dd33241a66ba79d67550edb
VuThiDiu <hoamayman2k@gmail.com>
2022-03-07 01:58:00 +0700

    add account page

--- src/main/java/com/example/demo/controller/AccountPage.java
+++ src/main/java/com/example/demo/controller/AccountPage.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class AccountPage {
     @GetMapping("/account/{id}")
     public String getHomePage(@PathVariable("id") String id){
-        System.out.println(id);
         return "account";
     }
 }
--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,27 +1,13 @@
 function HomePageController(){
+    // load data
     $(function loadData(){
         if(window.localStorage.getItem('loginResponse')!=null){
             var loginResponse = JSON.parse(localStorage.getItem("loginResponse"));
-//            $(".close").click(function(){
-//                var id = $(this).attr('id');
-//                var result = confirm("Want to delete?");
-//                        if (result==true) {
-//                            $.ajax({
-//                                method: "Delete",
-//                                url: "http://localhost:8080/todolist/"+id,
-//                                headers:{
-//                                    "Authorization": `Bearer ${loginResponse.accessToken}`,
-//                                }
-//                            }).done(function(){
-//                                alert("Delete successfully");
-//                            }).fail(function(){
-//
-//                            })
-//                        } else {
-//
-//                        }
-//
-            });
+        }else{
+            alert ("Login plz");
+            window.location.replace("/login");
+        }});
+
 }
 
 var homePageController;
